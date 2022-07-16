5243a67f2869921d39a94efcc85f5095c53744d2
JVPola <joaopola.123@gmail.com>
2022-07-16 13:12:52 -0300

    Atualização Login
    Separação em Funções MVC

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-usuario_digitado = input("Informe o seu usuario: ")
-senha_digitado = input("Informe sua senha: ")
+# View - o que vai para o Usuário
+def view():
+    usuario_digitado = input("Informe o seu usuario: ")
+    senha_digitado = input("Informe sua senha: ")
+    controller(usuario_digitado, senha_digitado)
 
 # Model - O que vem para o banco de dados (BD)
-usuario_BD = "joao"
-senha_BD = "123"
+def model_usuario():
+    usuario_BD = "joao"
+    return usuario_BD
 
+def model_senha():
+    senha_BD = "123"
+    return senha_BD
+    
 # Controller - A validação
-if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
-    print ("Pode entrar")
-else:
-    print("Usuário ou senha inválido")
+def controller(usuario_digitado, senha_digitado):
+    usuario_BD = model_usuario()
+    senha_BD = model_senha()
+    if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
+        print ("Pode entrar")
+    else:
+        print("Usuário ou senha inválido")
+
+view()
